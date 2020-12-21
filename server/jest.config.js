4292c36cb92c61b00a157cd3e997c91c62a3877e
kakao <key.h@kakaoenterprise.com>
2020-12-21 15:24:16 +0900

    setup typescript and lint

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,14 +1,14 @@
 module.exports = {
-  testEnvironment: "node",
+  testEnvironment: 'node',
   testEnvironmentOptions: {
-    NODE_ENV: "test",
+    NODE_ENV: 'test',
   },
   restoreMocks: true,
   coveragePathIgnorePatterns: [
-    "node_modules",
-    "src/config",
-    "src/app.js",
-    "tests",
+    'node_modules',
+    'src/config',
+    'src/app.js',
+    'tests',
   ],
-  coverageReporters: ["text", "lcov", "clover", "html"],
+  coverageReporters: ['text', 'lcov', 'clover', 'html'],
 };
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,9 +1,9 @@
-import express from "express";
+import express from 'express';
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.json({ x: 1, y: "a" });
+router.get('/', (req, res) => {
+  res.json({ x: 1, y: 'a' });
 });
 
 export default router;
