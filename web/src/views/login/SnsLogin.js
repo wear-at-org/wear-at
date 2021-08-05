ddf163774fbb75c9cbb4cf26633c0ba1864bb68d
DESKTOP-36B1Q7V\로컬 <rnwkfydwkd82@likelion.org>
2021-08-05 23:32:12 +0900

    Fix 카카오 이미지 오류 수정, 회원 가입 에러 수정

--- web/src/views/login/SnsLogin.js
+++ web/src/views/login/SnsLogin.js
@@ -40,7 +40,7 @@ const SnsLogin = () => {
           nickname: u.nickname ?? '',
           email: u.email ?? '',
           prividerType: u.provider ?? '',
-          profileImage: u.profileImage ?? '',
+          profileImage: u.profile_image ?? '',
         },
         loginStatus: 'login',
       }),
